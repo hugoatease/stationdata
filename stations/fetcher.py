2de16624feceb900d8d923948205a1245c224088
Hugo Caille <hugo.caille@isen-lille.fr>
2014-03-26 19:31:40 +0100

    Refactoring Fetcher in class and add fetch_requirements and fetch_until_score.

--- stations/fetcher.py
+++ stations/fetcher.py
@@ -1,53 +1,82 @@
 from station import Station, Mapper
 
 from . import icecast, shoutcast, icymeta
-adapters = [icymeta, shoutcast, icecast]
 
-def available_adapters():
-    available = []
+class Fetcher:
+    adapters = [icymeta, shoutcast, icecast]
 
-    for adapter in adapters:
-        if hasattr(adapter, 'adapter') and hasattr(adapter, 'mappings'):
-            available.append(adapter)
+    def __init__(self, url):
+        self.url = url
+        self.available_adapters = []
+        self.get_available_adapters()
 
-    return available
+    def get_available_adapters(self):
+        for adapter in self.adapters:
+            if hasattr(adapter, 'adapter') and hasattr(adapter, 'mappings'):
+                self.available_adapters.append(adapter)
 
-def adapter_fetch(adapter, url, weights):
-    station = Station()
+        return self.available_adapters
 
-    print "Adaptateur: " + adapter.name
+    def adapter_fetch(self, adapter):
+        current = Station()
 
-    stream = adapter.adapter(url)
-    try:
+        stream = adapter.adapter(self.url)
         result = stream.fetch()
-    except:
-        print "Erreur critique de l'adaptateur"
-        return None
 
-    mapper = Mapper(station, result)
-    mapper.mappings(adapter.mappings)
-    mapper.map()
+        mapper = Mapper(current, result)
+        mapper.mappings(adapter.mappings)
+        mapper.map()
 
-    score = station.score(weights)
+        return current
 
-    print "\tScore: " + str(score)
-    return (score, station)
+    def fetch_all(self):
+        result = Station()
 
-def fetch(url, weights={'track': 4}):
-    scores = {}
+        for adapter in self.available_adapters:
+            try:
+                result += self.adapter_fetch(adapter)
+            except:
+                pass
 
-    for adapter in available_adapters():
-        score = adapter_fetch(adapter, url, weights)
-        if score == None:
-            continue
-        else:
-            scores[score[0]] = score[1]
+        return result
 
-    print scores
+    def fetch_until_score(self, score, weights = {}):
+        result = Station()
 
-    best = sorted(scores, key=scores.get, reverse=True)[0]
-    result = scores[best]
+        for adapter in self.available_adapters:
+            print 'TEST'
+            try:
+                result += self.adapter_fetch(adapter)
+                if result.score(weights) >= score:
+                    break
+            except:
+                pass
 
-    print result.export()
+        return result
 
-    return result
+    def fetch_requirements(self, requirements):
+        result = Station()
+
+        for adapter in self.available_adapters:
+            try:
+                result += self.adapter_fetch(adapter)
+                if result.requires(requirements):
+                    break
+            except:
+                pass
+
+        return result
+
+    def fetch(self, score=None, requirements=None, weights=[]):
+        if score is None and requirements is None:
+            return self.fetch_all()
+
+        if score is not None:
+            return self.fetch_until_score(score, weights)
+
+        if requirements is not None:
+            return self.fetch_requirements(requirements)
+
+
+def fetch(url):
+    return Fetcher(url).fetch()
